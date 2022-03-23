faebf74b32de5e835cb3ec86615650a0d07c005a
Mikej81 <stumpj@gmail.com>
2022-03-23 12:51:56 -0400

    sites

--- xcs/xcs_site2.tf
+++ xcs/xcs_site2.tf
@@ -1,49 +1,48 @@
-resource "volterra_gcp_vpc_site" "example" {
-  name      = "acmecorp-web"
-  namespace = "staging"
-
-  // One of the arguments from this list "cloud_credentials assisted" must be set
+resource "volterra_gcp_vpc_site" "site2" {
+  name      = ""
+  namespace = "system"
 
   cloud_credentials {
     name      = "test1"
-    namespace = "staging"
-    tenant    = "acmecorp"
+    namespace = "system"
   }
+
   gcp_region    = ["us-west1"]
   instance_type = ["n1-standard-4"]
+  ssh_key       = ""
 
-  // One of the arguments from this list "log_receiver logs_streaming_disabled" must be set
+  logs_streaming_disabled = true
 
-  log_receiver {
-    name      = "test1"
-    namespace = "staging"
-    tenant    = "acmecorp"
-  }
-
-  // One of the arguments from this list "ingress_gw ingress_egress_gw voltstack_cluster" must be set
-
-  ingress_gw {
+  voltstack_cluster {
     gcp_certified_hw = "gcp-byol-voltmesh"
 
     gcp_zone_names = ["us-west1-a, us-west1-b, us-west1-c"]
 
-    local_network {
-      // One of the arguments from this list "new_network_autogenerate new_network existing_network" must be set
+    node_number = "3"
 
+    site_local_network {
       new_network_autogenerate {
         autogenerate = true
       }
     }
-
-    local_subnet {
-      // One of the arguments from this list "new_subnet existing_subnet" must be set
-
+    site_local_subnet {
       new_subnet {
-        primary_ipv4 = "10.1.0.0/16"
-        subnet_name  = "subnet1-in-network1"
+        subnet_name  = "subnet"
+        primary_ipv4 = "1.1.1.0/24"
       }
     }
 
-    node_number = "node_number"
+    global_network_list {
+      global_vn {
+
+      }
+    }
+
+    no_network_policy        = true
+    no_forward_proxy         = true
+    no_outside_static_routes = true
+    no_k8s_cluster           = true
+    default_storage          = ""
   }
+
 }

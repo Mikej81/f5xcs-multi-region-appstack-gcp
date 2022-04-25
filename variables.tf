7360984c1443616968b85948fa285993792b8764
Nettas <nettas@aol.com>
2022-04-25 08:30:13 -0400

    updates

--- variables.tf
+++ variables.tf
@@ -86,7 +86,8 @@ variable "zone_three_c" {
 variable "instance_type" {
   type        = string
   description = "REQUIRED: Instance Type"
-  default     = "n1-standard-4"
+  #default     = "n1-standard-4"
+  default     = "n1-standard-8"
 }
 
 variable "sshPublicKey" {

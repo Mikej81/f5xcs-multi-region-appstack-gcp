4e12fd53f5f63aac21daddb2511863c5a14dac96
Mikej81 <stumpj@gmail.com>
2023-04-19 16:28:28 -0400

    agility fixes: race condition

--- xcs/xcs.tf
+++ xcs/xcs.tf
@@ -8,6 +8,10 @@ terraform {
   }
 }
 
+resource "volterra_namespace" "namespace" {
+  name = var.namespace
+}
+
 resource "volterra_cloud_credentials" "gcp_cc" {
   name      = format("%s-gcp-credentials", var.name)
   namespace = "system"
@@ -18,9 +22,7 @@ resource "volterra_cloud_credentials" "gcp_cc" {
         url = format("string:///%s", base64encode(file("${path.module}/../key.json")))
       }
     }
-
   }
-
 }
 
 resource "volterra_virtual_network" "global" {
@@ -52,44 +54,8 @@ resource "volterra_k8s_cluster" "cluster" {
   namespace = "system"
 
   no_cluster_wide_apps              = true
-  use_default_cluster_role_bindings = false
-  use_default_cluster_roles         = false
-  use_custom_cluster_role_list {
-    cluster_roles {
-      namespace = "system"
-      name      = "admin"
-    }
-    cluster_roles {
-      namespace = "system"
-      name      = "ves-io-psp-permissive"
-    }
-    cluster_roles {
-      namespace = "system"
-      name      = "ves-io-admin-cluster-role"
-    }
-    cluster_roles {
-      namespace = "system"
-      name      = var.name
-    }
-  }
-  use_custom_cluster_role_bindings {
-    cluster_role_bindings {
-      namespace = "system"
-      name      = "admin"
-    }
-    cluster_role_bindings {
-      namespace = "system"
-      name      = "ves-io-admin-cluster-role-binding"
-    }
-    cluster_role_bindings {
-      namespace = "shared"
-      name      = "ves-io-psp-permissive"
-    }
-    cluster_role_bindings {
-      namespace = "system"
-      name      = var.name
-    }
-  }
+  use_default_cluster_role_bindings = true
+  use_default_cluster_roles         = true
 
 
   // One of the arguments from this list "cluster_scoped_access_permit cluster_scoped_access_deny" must be set
@@ -103,52 +69,6 @@ resource "volterra_k8s_cluster" "cluster" {
   }
   use_default_psp = true
 }
-###create cluster role ###
-resource "volterra_k8s_cluster_role" "role" {
-  name = var.name
-  # name      = var.name2
-  namespace = "system"
-
-  // One of the arguments from this list "policy_rule_list k8s_cluster_role_selector yaml" must be set
-
-  policy_rule_list {
-    policy_rule {
-      // One of the arguments from this list "resource_list non_resource_url_list" must be set
-      resource_list {
-
-        verbs = ["*"]
-
-        api_groups = ["*"]
-
-        resource_types = ["*"]
-
-        resource_instances = ["*"]
-      }
-      # non_resource_url_list {
-      #   urls = ["*"]
-
-      #   verbs = ["*"]
-      # }
-    }
-  }
-}
-###create cluster role binding###
-resource "volterra_k8s_cluster_role_binding" "bind" {
-  name = var.name
-  # name      = var.name2
-  namespace = "system"
-
-  k8s_cluster_role {
-    name      = "ves-io-admin-cluster-role"
-    namespace = "shared"
-    tenant    = "ves-io"
-  }
-
-  subjects {
-    // One of the arguments from this list "user service_account group" must be set
-    user = "s.iannetta@f5.com"
-  }
-}
 
 resource "volterra_virtual_site" "vsite" {
   name      = format("%s-k8s-vsite", var.name)
@@ -161,30 +81,14 @@ resource "volterra_virtual_site" "vsite" {
   site_type = "CUSTOMER_EDGE"
 }
 
-## Coleman added / edited for Agility
-# resource "volterra_namespace" "namespace" {
-#   count = var.agility_namespaces
-#   name  = "3${format("%02d", count.index)}"
-
-# }
-
-# resource "volterra_virtual_k8s" "vk8s" {
-#   count     = var.agility_namespaces
-#   name      = format("%s-vk8s-3${format("%02d", count.index)}", var.name)
-#   namespace = "3${format("%02d", count.index)}"
-
-#   vsite_refs {
-#     name      = volterra_virtual_site.vsite.name
-#     namespace = "shared"
-#   }
-# }
-# ## End Coleman Added / Edited
-
-
 resource "volterra_virtual_k8s" "vk8s" {
-  name = format("%s-vk8s", var.name)
-  # namespace = var.namespace
-  namespace = var.namespace
+  depends_on = [
+    volterra_namespace.namespace, volterra_virtual_site.vsite, volterra_gcp_vpc_site.site1, volterra_gcp_vpc_site.site2, volterra_gcp_vpc_site.site3
+  ]
+  namespace = volterra_namespace.namespace.name
+  name      = format("%s-vk8s", var.name)
+
+
   vsite_refs {
     name      = volterra_virtual_site.vsite.name
     namespace = "shared"
